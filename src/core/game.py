c4b3a0baccdb19a947d14840169bcaa81c0aecf4
Quertzy <99974252+MyAngelShibuyaKanon@users.noreply.github.com>
2025-12-15 18:26:13 +0800

    add comments to character.py and fix int stuff

--- src/core/game.py
+++ src/core/game.py
@@ -10,12 +10,21 @@ from features.effects.effects import Effect
 
 class Game:
     def start(self):
+        """
+        Starts the "Game"/ DMG calculator
+        """
+        # Initialize actions
         normalAttack = NormalAttack()
         typeAttack = TypeAttack()
+
+        # Initialize 2 chatacters, dict is used to store character info and is unpacked to
+        # be passed as an argument
         print("### Attacker Setup ###")
         attacker = Character(**self.getCharInfo(isAttacker=True))
         print("### Target Setup ###")
         target = Character(**self.getCharInfo(askEvade=True, isAttacker=False))
+
+        # Simple attack flow using if-else
         print("### Choose Attack ###")
         if self.isAttackNormal("Attack Type (Normal/Type): "):
             dmgString = normalAttack.execute(attacker, target)
@@ -26,29 +35,41 @@ class Game:
         pass
 
     def getCharInfo(self, isAttacker: bool, askEvade: bool = False) -> dict:
+        """
+        Obtains character info by storing it in a dataclass CharacterInfo and returning
+        it as a dictionary. I love dataclasses.
+        """
         charInfo = CharacterInfo()
         charInfo.name = input(f"{'Attacker' if isAttacker else 'Target'} Name: ")
-        charInfo.HP = int(input("HP: "))
-        charInfo.ATK = int(input("ATK: "))
-        charInfo.DEF = int(input("DEF: "))
+        charInfo.HP = self.getInt("HP: ")
+        charInfo.ATK = self.getInt("ATK: ")
+        charInfo.DEF = self.getInt("DEF: ")
         charInfo.charType = self.getType()
-        charInfo.weaponAtk = int(input("Weapon ATK bonus: "))
-        charInfo.armorDef = int(input("Armor DEF bonus: "))
+        charInfo.weaponAtk = self.getInt("Weapon ATK bonus: ")
+        charInfo.armorDef = self.getInt("Armor DEF bonus: ")
         print(f"Applying {'Buffs' if isAttacker else 'Debuffs'} (50%)")
         charInfo.buffs, charInfo.debuffs = self.getEffects(askEvade=askEvade)
 
         return asdict(charInfo)
 
     def getType(self) -> CharacterType:
+        """
+        Gets CharacterType (Fire, Earth, etc) from input() in a error-handled way and returns
+        it with the correct type
+        """
         while True:
             try:
                 return CharacterType[
                     input("Type(Fire/Earth/Water/Light/Dark): ").upper()
-                ]
+                ]  # This gets the correct type object from the CharacterType ENUM
             except:
                 print("Invalid character type!")
 
     def getBool(self, string: str) -> bool:
+        """
+        Gets Bool from input() through y/n and returns
+        it with the correct type
+        """
         while True:
             match input(string).lower():
                 case "y":
@@ -59,6 +80,10 @@ class Game:
                     print("Invalid input")
 
     def isAttackNormal(self, string: str) -> bool:
+        """
+        Gets Bool through input() and returns
+        it with the correct type
+        """
         while True:
             match input(string).lower():
                 case "normal":
@@ -69,6 +94,10 @@ class Game:
                     print("Invalid input")
 
     def getInt(self, string: str) -> int:
+        """
+        Gets int from input() and returns it
+        with the correct type
+        """
         while True:
             try:
                 return int(input(string))
@@ -76,6 +105,11 @@ class Game:
                 print("Input not an int")
 
     def getEffects(self, askEvade: bool) -> tuple[list[Effect], list[Effect]]:
+        """
+        Gets list of buffs and effects through a series of getBools,
+        asking if a certain buff/debuff should be added. returns list of buff and
+        list of debuffs wrapped in tuple
+        """
         buffs: list[Effect] = []
         debuffs: list[Effect] = []
         if self.getBool("Apply HP Buff? (y/n): "):
